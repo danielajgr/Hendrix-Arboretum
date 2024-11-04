246f3e1573b6da7353386f5013da537fbf37871b
Amelia Jones <jonesas@hendrix.edu>
2024-11-04 16:50:55 -0600

    Tree info page

--- final_project/lib/pages/tree_info.dart
+++ final_project/lib/pages/tree_info.dart
@@ -1,24 +1,28 @@
 import 'package:flutter/material.dart';
+//import 'package:final_project/lib/api/tree.dart';
 
 class TreeInfo extends StatefulWidget {
-  const TreeInfo({super.key});
+  TreeInfo({super.key, required this.treeid});
   //will need to know the tree?
+  final int treeid;
   @override
   State<TreeInfo> createState() => _TreeInfoState();
 }
 
 class _TreeInfoState extends State<TreeInfo> {
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(appBar: AppBar
     (title: const Text("Tree Info"),
     ),
-      body: const SingleChildScrollView(child: 
-        Column(children: 
-          [
-          Text('Very nice tree!')
+      body: ListView(padding: const EdgeInsets.all(50), children: [
+      
+          
+          
+          const Text('Very nice tree!', textAlign: TextAlign.center),
+          Container( height: 200, decoration: const BoxDecoration(color: Colors.green)),
           ],
-        ),
       ),
     );
   }
