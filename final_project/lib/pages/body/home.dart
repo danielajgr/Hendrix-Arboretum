92e76144cc87d5d5201cd6e15206df91e4f055f8
JustinGrubbs <grubbsjr@hendrix.edu>
2024-11-10 14:12:35 -0600

    Added basic map

--- final_project/lib/pages/body/home.dart
+++ final_project/lib/pages/body/home.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import "package:flutter_map/flutter_map.dart";
+import "package:latlong2/latlong.dart";
 
 class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        Text("cool map here")
-      ]
-    );
-  }
+   @override
+   Widget build(BuildContext context) {
+      return FlutterMap(
+         options: MapOptions(
+            initialCenter: LatLng(35.100284, -92.441645),
+            initialZoom: 16
+         ),
+         // https://docs.fleaflet.dev/
+         children: [
+            TileLayer(
+               urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // OSMF's Tile Server
+               userAgentPackageName: 'com.example.app',
+            )
+         ]
+      );
+   }
 }
