2b174b149ebc49341b78655862d8fce3d0fbfbb6
JustinGrubbs <grubbsjr@hendrix.edu>
2024-11-10 15:30:27 -0600

    Fixed size of map and added space for filtering

--- final_project/lib/pages/body/home.dart
+++ final_project/lib/pages/body/home.dart
@@ -3,20 +3,46 @@ import "package:flutter_map/flutter_map.dart";
 import "package:latlong2/latlong.dart";
 
 class Home extends StatelessWidget {
+
    @override
    Widget build(BuildContext context) {
-      return FlutterMap(
-         options: MapOptions(
-            initialCenter: LatLng(35.100284, -92.441645),
-            initialZoom: 16
-         ),
-         // https://docs.fleaflet.dev/
-         children: [
-            TileLayer(
-               urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // OSMF's Tile Server
-               userAgentPackageName: 'com.example.app',
-            )
-         ]
+      return LayoutBuilder(
+         builder: (context, constraints) {
+            return Column(   
+               children: [
+
+                  // Filtering options eventually
+                  const FloatingActionButton(
+                     onPressed: null
+                  ),
+                  const FloatingActionButton(
+                     onPressed: null
+                  ),
+                  const FloatingActionButton(
+                     onPressed: null
+                  ),
+
+                  Expanded(
+                     child: FlutterMap(
+                        options: const MapOptions(
+                           initialCenter: LatLng(35.100232, -92.440290),
+                           // initialCenter: LatLng(35.100284, -92.440724),
+                           // initialCenter: LatLng(35.100284, -92.441645),
+                           initialZoom: 16
+                        ),
+                        children: [
+                           TileLayer(
+                              // https://docs.fleaflet.dev/
+                              // Tilemap can be changed eventually
+                              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                              userAgentPackageName: 'com.example.app',
+                           )
+                        ]
+                     )
+                  )
+               ]
+            );
+         }
       );
    }
 }
