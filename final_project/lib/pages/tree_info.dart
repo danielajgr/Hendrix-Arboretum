c60fa10c3704b32c7da4da0843b7f2bc70a7ea57
Amelia Jones <jonesas@hendrix.edu>
2024-11-11 03:16:09 -0600

    Tree info page shows tree info
    The code looks bad

--- final_project/lib/pages/tree_info.dart
+++ final_project/lib/pages/tree_info.dart
@@ -1,5 +1,8 @@
+import 'package:final_project/api/tree.dart';
 import 'package:flutter/material.dart';
-//import 'package:final_project/lib/api/tree.dart';
+
+
+
 
 class TreeInfo extends StatefulWidget {
   TreeInfo({super.key, required this.treeid});
@@ -9,21 +12,102 @@ class TreeInfo extends StatefulWidget {
   State<TreeInfo> createState() => _TreeInfoState();
 }
 
+/*FutureBuilder<Tree>(
+  future: fetchTree,
+  builder: (context, snapshot) {
+    if(snapshot.hasData){
+
+    }
+  }
+)**/
+
 class _TreeInfoState extends State<TreeInfo> {
-  
+  late Future<Tree> futureTree;
+  @override
+  void initState() {
+    super.initState();
+    futureTree = fetchTree(widget.treeid);
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar
-    (title: const Text("Tree Info"),
+    return Scaffold( 
+      backgroundColor: const Color.fromARGB(255, 175, 225, 175),
+      appBar: AppBar
+    (title: Text("Tree #${widget.treeid}", style: Theme.of(context).textTheme.displayMedium), backgroundColor: Color.fromARGB(255, 0, 103, 79), actions: [
+      IconButton(icon: Icon(Icons.favorite),
+        onPressed: () async {
+            },
+            )]
     ),
       body: ListView(padding: const EdgeInsets.all(50), children: [
-      
+
           
+           FutureBuilder<Tree>(
+              future: futureTree,
+              builder: (context, snapshot) {
+              if(snapshot.hasData){
+                return Image.network(snapshot.data!.imageURL);
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+                return Text('');
+              },
+           ),
+
+           Card(color: Color.fromARGB(255, 0, 103, 79), child: Column(children: [
+             const Text('Common Name', textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontSize: 18) /*style: TextStyle(),**/),
+            FutureBuilder<Tree>(
+              future: futureTree,
+              builder: (context, snapshot) {
+              if(snapshot.hasData){
+                return Text(snapshot.data!.commonName, style: TextStyle(color: Colors.white));
+              }else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+                return Text('');
+              }
+              ),
+           
+            //const Text('Pine, Loblolly', textAlign: TextAlign.center, style: TextStyle(color: Colors.white)),
+          ],)
+          ),
+           Card(color: Color.fromARGB(255, 0, 103, 79), child: Column(children: [
+            const Text('Scientific Name', textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontSize: 18) /*style: TextStyle(),**/),
+            FutureBuilder<Tree>(
+              future: futureTree,
+              builder: (context, snapshot) {
+              if(snapshot.hasData){
+                return Text(snapshot.data!.scientificName, style: TextStyle(color: Colors.white));
+              }else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+                return Text('');
+              }
+              ),
+          ],)
+          ),
+
+        Card(color: Color.fromARGB(255, 0, 103, 79), child: Column(children: [
+            const Text('Building', textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontSize: 18) /*style: TextStyle(),**/),
+            FutureBuilder<Tree>(
+              future: futureTree,
+              builder: (context, snapshot) {
+              if(snapshot.hasData){
+                return Text(snapshot.data!.buildingName, style: TextStyle(color: Colors.white));
+              }else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+                return Text('');
+              }
+              ),
+          ],)
+          )
           
-          const Text('Very nice tree!', textAlign: TextAlign.center),
-          Container( height: 200, decoration: const BoxDecoration(color: Colors.green)),
           ],
       ),
+
+      
     );
   }
 }
+
