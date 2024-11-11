2adf3f71aad7624d012832a7c805da4f258a470d
JustinGrubbs <grubbsjr@hendrix.edu>
2024-11-10 18:51:53 -0600

    Added markers to map

--- final_project/lib/pages/body/home.dart
+++ final_project/lib/pages/body/home.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import "package:flutter_map/flutter_map.dart";
-import "package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart";
 import "package:latlong2/latlong.dart";
 import "/api/tree.dart";
 
 class Home extends StatefulWidget {
+
    @override 
    _HomeState createState() => _HomeState();
 }
@@ -19,23 +19,24 @@ class _HomeState extends State<Home> {
       builder: (context, constraints) {
          return Column(   
             children: [
-
-               Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: Row(
-                     children: [
-                        Text("Search by tree ID:  "),
-                        Container(  
-                           width: 200,
-                           // Change to TextBox
-                           child: TextField( 
-                              onSubmitted: (id) {
-                                 searchTree(id);
-                              },
-                           )
+               Row(children: [
+                  Text("Search by Tree ID:  "),
+                  Padding(
+                     padding: const EdgeInsets.all(8),
+                     child: SizedBox(
+                        width: 150,
+                        height: 60,
+                        child: TextField( 
+                           decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                           ),
+                           onSubmitted: (id) {
+                              searchTree(id);
+                           },
                         )
-                     ]
-                  )
+                     )
+                  ),
+               ],
                ),
 
                Expanded(
@@ -47,10 +48,25 @@ class _HomeState extends State<Home> {
                      children: [
                         TileLayer(
                            // https://docs.fleaflet.dev/
-                           // Tilemap can be changed eventually
-                           urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                           userAgentPackageName: 'com.example.app',
-                        )
+                           urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png' // Should change
+                        ),
+                        if (treeLocation != null) MarkerLayer(
+                           markers: [
+                              Marker(
+                                 point: treeLocation!,
+                                 width: 60,
+                                 height: 60,
+                                 child: Container(
+                                    alignment: Alignment.center,
+                                    child: const Icon(
+                                       Icons.location_on,
+                                       color: Color.fromARGB(255, 202, 81, 39),
+                                       size: 50
+                                    ),
+                                 ),
+                              ),
+                           ],
+                        ),
                      ]
                   )
                )
@@ -62,16 +78,15 @@ class _HomeState extends State<Home> {
 
    Future<void> searchTree(String id) async {
       try {
-      Tree tree = await fetchTree(int.parse(id));
-      setState(() {
-         treeLocation = LatLng(
-            double.parse(tree.latitude),
-            double.parse(tree.longitude),
-         );
-         print(treeLocation); // Show fetch works
-      });
-    } catch (e) {
-      print("Error fetching tree: $e");
-    }
+         Tree tree = await fetchTree(int.parse(id));
+         setState(() {
+            treeLocation = LatLng(
+               double.parse(tree.latitude),
+               double.parse(tree.longitude),
+            );
+         });
+      } catch (e) {
+         print("Error fetching tree: $e");
+      }
    }
 }
