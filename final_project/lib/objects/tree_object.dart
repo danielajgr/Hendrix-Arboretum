763f54f0937b6aeb5bf10b59ee9f2df0390f022a
LeitMoth <col.phillips2004@gmail.com>
2024-11-11 12:34:10 -0600

    update tree_object and add back http dependency

--- final_project/lib/objects/tree_object.dart
+++ final_project/lib/objects/tree_object.dart
@@ -1,9 +1,15 @@
-class TreeObject{
+import '../api/tree.dart';
+
+class TreeObject {
   TreeObject({required this.treeid});
   int treeid;
   int likes = 0;
 
-  void add_like(){
-    likes ++;
+  void addLike() {
+    likes++;
+  }
+
+  Future<Tree> getTree() {
+    return fetchTree(treeid);
   }
 }
