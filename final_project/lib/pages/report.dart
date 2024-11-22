7feaf5892be071f2651654a1b41f46566f20591c
danielajgr <daniela92602@gmail.com>
2024-11-21 23:25:04 -0600

    fixed url android bug and stuff
    also made the basic report page, ugly right now but it works, I'll fix it before we show clients

--- final_project/lib/pages/report.dart
+++ final_project/lib/pages/report.dart
@@ -1,5 +1,6 @@
 import 'package:final_project/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Report extends StatefulWidget{
   Report({super.key});
@@ -10,14 +11,37 @@ class Report extends StatefulWidget{
 }
 
 class _ReportState extends State<Report> {
+
+  void _launchURL(String url) async {
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 175, 225, 175),
       appBar: AppBar(
-        title: Header("Report")
+        title: Text("Profile",style: Theme.of(context).textTheme.displayMedium),
+        backgroundColor: const Color.fromARGB(255, 0, 103, 79),
+        
       ),
-      body: Text('Coming Soon!')   
+      body: Column( crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [SizedBox(height: 16),
+                              Text("Is there a missing tag?", style: Theme.of(context).textTheme.headlineLarge),
+                              SizedBox(height: 16),
+                              Text("Is there a damaged tag?",style: Theme.of(context).textTheme.headlineLarge),
+                              SizedBox(height: 16),
+                              Text("Click the button below to report any concerns",style: Theme.of(context).textTheme.headlineLarge, textAlign: TextAlign.center),
+                              SizedBox(height: 16,),
+                              ElevatedButton(child: Text("Report"),
+                              onPressed: () {
+                                _launchURL('https://forms.gle/QnkWZRu1437LN3RYA'); 
+                              }, )
+                              ])  
     );
     
     
