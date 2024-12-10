3caf4ffa69ff344e5c626f576a07d4b2ec304723
LeitMoth <col.phillips2004@gmail.com>
2024-12-10 16:26:33 -0600

    fix "Send Verification Email" overflow by override a specific part of the theme

--- final_project/lib/pages/profile.dart
+++ final_project/lib/pages/profile.dart
@@ -19,60 +19,87 @@ class _ProfileState extends State<Profile> {
   Widget build(BuildContext context) {
     return AppBarWrapper(
       title: "Profile",
-      body: ProfileScreen(providers: const [], actions: [
-        SignedOutAction((context) {
-          context.pushReplacement('/');
-        }),
-      ], children: [
-        const Padding(padding: EdgeInsets.all(5)),
-        Container(
-          padding: const EdgeInsets.all(1),
-          child: const TextAndIcon(Icons.forest, "Your Liked Trees", 30),
+      // https://docs.flutter.dev/cookbook/design/themes
+      body: Theme(
+        // NOTE(LeitMoth):
+        // This theme modification is necessary to prevent the
+        // "Send Verification Email" button from overflowing the view.
+        // I can't modify ProfileScreen but the I can modify
+        // the part of the theme that made to button too large:
+        data: Theme.of(context).copyWith(
+          textTheme: Theme.of(context).textTheme.copyWith(
+                labelLarge: const TextStyle(
+                  // Used for dropdown menu text and search bar labels.
+                  // And, importantly, the "Send Verification Email" button
+                  // This also affects the sign out and delete account buttons,
+                  // but there is not much I can do about that.
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Museo',
+                  // Doesn't overflow, and gives a little wiggle room for
+                  // skinnier phones. Update if you want, but update with care.
+                  fontSize: 18,
+                ),
+              ),
         ),
-        const Padding(padding: EdgeInsets.only(top: 10)),
-        Consumer<ApplicationState>(
-          builder: (context, appState, _) => ListView(
-              padding: EdgeInsets.all(0.8),
-              shrinkWrap: true,
-              children: appState.getAll().map((item) {
-                return ListTile(
-                  shape: const Border(
-                    top: BorderSide(
-                        color: Color.fromARGB(255, 134, 72, 20), width: 1),
-                  ),
-                  title: Text("Tree #" + item.treeid.toString()),
-                  trailing: IconButton(
-                    icon: Icon(Icons.favorite),
-                    color: appState.isFavorite(item.treeid)
-                        ? Color.fromARGB(255, 255, 0, 0)
-                        : Color(0xff9A9A9A),
-                    onPressed: () async {
-                      if (!appState.loggedIn) {
-                        context.push('/sign-in');
-                      } else {
-                        if (appState.isFavorite(item.treeid)) {
-                          setState(() {
-                            appState.removeTree(item.treeid);
-                          });
-                        } else {
-                          setState(() {
-                            appState.addTree(item.treeid);
-                          });
-                        }
-                      }
-                    },
-                  ),
-                  onTap: () async {
-                    await Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => TreeInfo(treeid: item.treeid),
+        child: ProfileScreen(
+          providers: const [],
+          actions: [
+            SignedOutAction((context) {
+              context.pushReplacement('/');
+            }),
+          ],
+          children: [
+            const Padding(padding: EdgeInsets.all(5)),
+            Container(
+              padding: const EdgeInsets.all(1),
+              child: const TextAndIcon(Icons.forest, "Your Liked Trees", 30),
+            ),
+            const Padding(padding: EdgeInsets.only(top: 10)),
+            Consumer<ApplicationState>(
+              builder: (context, appState, _) => ListView(
+                  padding: EdgeInsets.all(0.8),
+                  shrinkWrap: true,
+                  children: appState.getAll().map((item) {
+                    return ListTile(
+                      shape: const Border(
+                        top: BorderSide(
+                            color: Color.fromARGB(255, 134, 72, 20), width: 1),
                       ),
+                      title: Text("Tree #" + item.treeid.toString()),
+                      trailing: IconButton(
+                        icon: Icon(Icons.favorite),
+                        color: appState.isFavorite(item.treeid)
+                            ? Color.fromARGB(255, 255, 0, 0)
+                            : Color(0xff9A9A9A),
+                        onPressed: () async {
+                          if (!appState.loggedIn) {
+                            context.push('/sign-in');
+                          } else {
+                            if (appState.isFavorite(item.treeid)) {
+                              setState(() {
+                                appState.removeTree(item.treeid);
+                              });
+                            } else {
+                              setState(() {
+                                appState.addTree(item.treeid);
+                              });
+                            }
+                          }
+                        },
+                      ),
+                      onTap: () async {
+                        await Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => TreeInfo(treeid: item.treeid),
+                          ),
+                        );
+                      },
                     );
-                  },
-                );
-              }).toList()),
+                  }).toList()),
+            ),
+          ],
         ),
-      ]),
+      ),
     );
   }
 }
