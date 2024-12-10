45943d74268045ea633dc0e47b7bef657588ae18
LeitMoth <col.phillips2004@gmail.com>
2024-12-10 15:23:24 -0600

    refactor report to use AppBarWrapper + fix mistake with report page name

--- final_project/lib/pages/report.dart
+++ final_project/lib/pages/report.dart
@@ -2,6 +2,8 @@ import 'package:final_project/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+import '../widgets/authentication.dart';
+
 class Report extends StatefulWidget {
   Report({super.key});
 
@@ -20,34 +22,30 @@ class _ReportState extends State<Report> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: const Color.fromARGB(255, 175, 225, 175),
-        appBar: AppBar(
-          title:
-              Text("Profile", style: Theme.of(context).textTheme.displayMedium),
-          backgroundColor: const Color.fromARGB(255, 0, 103, 79),
+    return AppBarWrapper(
+      title: "Report A Problem",
+      body: ListView(children: [
+        SizedBox(height: 16),
+        Text("Is there a missing tag?",
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.headlineLarge),
+        Image.asset('assets/missing.PNG'),
+        Text("Is there a damaged tag?",
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.headlineLarge),
+        Image.asset('assets/dam.PNG'),
+        Text("Click the button below to report any concerns",
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.headlineLarge),
+        SizedBox(height: 16),
+        ElevatedButton(
+          child: Text("Report"),
+          onPressed: () {
+            _launchURL('https://forms.gle/QnkWZRu1437LN3RYA');
+          },
         ),
-        body: ListView(children: [
-          SizedBox(height: 16),
-          Text("Is there a missing tag?",
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.headlineLarge),
-          Image.asset('assets/missing.PNG'),
-          Text("Is there a damaged tag?",
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.headlineLarge),
-          Image.asset('assets/dam.PNG'),
-          Text("Click the button below to report any concerns",
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.headlineLarge),
-          SizedBox(height: 16),
-          ElevatedButton(
-            child: Text("Report"),
-            onPressed: () {
-              _launchURL('https://forms.gle/QnkWZRu1437LN3RYA');
-            },
-          ),
-          SizedBox(height: 16)
-        ]));
+        SizedBox(height: 16)
+      ]),
+    );
   }
 }
