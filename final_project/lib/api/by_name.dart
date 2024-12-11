e20bc1ac0dd0c30b0d6faef5426ed19f16efc6d7
LeitMoth <col.phillips2004@gmail.com>
2024-12-11 17:16:51 -0600

    add fetchTreeBySpecies function

--- final_project/lib/api/by_name.dart
+++ final_project/lib/api/by_name.dart
@@ -1,3 +1,8 @@
+import 'dart:convert';
+
+import 'tree.dart';
+import 'package:http/http.dart' as http;
+
 class SpeciesName {
   final int id;
   final String name;
@@ -9,5 +14,31 @@ class SpeciesName {
       {'id': int id, 'name': String name} => SpeciesName(id: id, name: name),
       _ => throw FormatException('Failed to load SpeciesName: $json'),
     };
+  }
+}
+
+Future<List<Tree>> fetchTreesBySpecies(bool scientificMode, String name) async {
+  final category = scientificMode ? "Scientific" : "Common";
+  final response = await http.get(
+      Uri.parse('https://arboretum.hendrix.edu/API/$category/?name=$name'));
+
+  if (response.statusCode == 200) {
+    var dec = jsonDecode(response.body);
+    if (dec case List<dynamic> jlist) {
+      List<Tree> trees = [];
+      for (var entry in jlist) {
+        if (entry case Map<String, dynamic> jobject) {
+          trees.add(Tree.fromJson(jobject));
+        } else {
+          throw Exception('Expected json object, got this instead: $entry');
+        }
+      }
+      return trees;
+    } else {
+      throw Exception('Expected JSON list, got this instead: $dec');
+    }
+  } else {
+    throw Exception(
+        'Failed to load trees by name \'$name\', in category \'$category\' HTTP status: ${response.statusCode}');
   }
 }
